ae9b5d16aec684d55776918b005c0cdcd82e510a
spacelocust <lucasboganin@hotmail.fr>
2021-03-08 09:05:18 +0100

    feat/add-entity

---
+++ src/Entity/Document.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DocumentRepository::class)
+ */
+class Document
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lien;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creer;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modifier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): self
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreer(): ?\DateTimeInterface
+    {
+        return $this->creer;
+    }
+
+    public function setCreer(\DateTimeInterface $creer): self
+    {
+        $this->creer = $creer;
+
+        return $this;
+    }
+
+    public function getModifier(): ?\DateTimeInterface
+    {
+        return $this->modifier;
+    }
+
+    public function setModifier(\DateTimeInterface $modifier): self
+    {
+        $this->modifier = $modifier;
+
+        return $this;
+    }
+}
