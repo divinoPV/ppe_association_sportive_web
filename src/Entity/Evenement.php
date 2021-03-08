398ca6f52c676e45fc8093ca47698c94c0a29fc9
lauraagss <laura.goncalves@lyceestvincent.fr>
2021-03-08 09:11:46 +0100

    feat/add-entity

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EvenementRepository::class)
+ */
+class Evenement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $debut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fin;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creer;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modifier;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nombrePlaces;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $vignette;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getCreer(): ?\DateTimeInterface
+    {
+        return $this->creer;
+    }
+
+    public function setCreer(\DateTimeInterface $creer): self
+    {
+        $this->creer = $creer;
+
+        return $this;
+    }
+
+    public function getModifier(): ?\DateTimeInterface
+    {
+        return $this->modifier;
+    }
+
+    public function setModifier(\DateTimeInterface $modifier): self
+    {
+        $this->modifier = $modifier;
+
+        return $this;
+    }
+
+    public function getNombrePlaces(): ?int
+    {
+        return $this->nombrePlaces;
+    }
+
+    public function setNombrePlaces(int $nombrePlaces): self
+    {
+        $this->nombrePlaces = $nombrePlaces;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getVignette(): ?string
+    {
+        return $this->vignette;
+    }
+
+    public function setVignette(string $vignette): self
+    {
+        $this->vignette = $vignette;
+
+        return $this;
+    }
+}
