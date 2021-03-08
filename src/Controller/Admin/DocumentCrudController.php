e8fbb6537c9bdd6c389dd92d498f67cade8b2463
lauraagss <laura.goncalves@lyceestvincent.fr>
2021-03-08 22:30:45 +0100

    add(crud Pages)

---
+++ src/Controller/Admin/DocumentCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Document;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class DocumentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Document::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnform(),
+            TextField::new('nom'),
+            TextField::new('lien'),
+            TextField::new('description'),
+            TextField::new('creer')->hideOnForm(),
+            TextField::new('modifier')->hideOnForm(),
+            AssociationField::new('DocumentCategory')->autocomplete()
+        ];
+    }
+
+}
