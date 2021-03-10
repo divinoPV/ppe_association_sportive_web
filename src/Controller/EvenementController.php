fe266948b58db87f9d591ec0ae405fe0648ad07b
divinoPV <monteiro.hugo2001@icloud.com>
2021-03-10 12:07:50 +0100

    build(events): form, template, controller

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use App\Form\EvenementSearchType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
     #[Route('/evenement', name: 'evenement')]
-    public function index(): Response
+    public function index(EntityManagerInterface $manager,
+                          Request $request
+    ): Response
     {
+        $qb = $manager
+            ->createQueryBuilder()
+            ->select('count(event.id)')
+            ->from('App:Evenement', 'event');
+        $count = $qb->getQuery()->getSingleScalarResult();
+
+        $events = $manager
+            ->getRepository(Evenement::class)
+            ->findAll();
+
+        $searchEventForm = $this->createForm(EvenementSearchType::class);
+
+
         return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
+            'events' => $events,
+            'count' => $count,
+            'search_event_form' => $searchEventForm->createView()
         ]);
     }
 }
---
+++ src/Form/EvenementSearchType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Evenement;
+use App\Entity\Sport;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sport', EntityType::class, [
+                'class' => Sport::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('s')
+                        ->orderBy('s.nom', 'ASC');
+                },
+                'choice_label' => 'nom',
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.nom', 'ASC');
+                },
+                'choice_label' => 'nom',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
