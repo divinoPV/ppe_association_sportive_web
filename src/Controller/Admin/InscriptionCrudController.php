4c44b7dcb555cb04f7e897fd381e454d0b14c272
divino <monteiro.hugo2001@icloud.com>
2021-03-27 18:12:09 +0100

    build(app)[controller]{crud}/inscription, dashboard\: optimization (27/03/21)/{v2};

--- src/Controller/Admin/InscriptionCrudController.php
+++ src/Controller/Admin/InscriptionCrudController.php
@@ -2,14 +2,11 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Categorie;
 use App\Entity\Inscription;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 /**
  * IMPORTANT !
