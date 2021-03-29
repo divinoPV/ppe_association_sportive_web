1e24c6cfda2c558cf53b90fe7f5fdcf8e1cfc606
spacelocust <lucasboganin@hotmail.fr>
2021-03-29 15:55:33 +0200

    build(profil)[template]

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class UtilisateurController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user/{id}", name="profil")
      */
-    public function index(): Response
+    public function profil(User $user): Response
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'user' => $user,
         ]);
     }
 }
