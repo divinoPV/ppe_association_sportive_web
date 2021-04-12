23acfe1377eb6b814f047f4cc71881b7e59d342d
lauraagss <laura.goncalves@lyceestvincent.fr>
2021-04-12 12:02:00 +0200

    build(profil)[style]

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Inscription;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +15,15 @@ class UtilisateurController extends AbstractController
      */
     public function profil(User $user): Response
     {
+        $inscriptions = $this
+            ->getDoctrine()
+            ->getRepository(Inscription::class)
+            ->findBy(['utilisateur' => $user]);
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
             'user' => $user,
+            'inscriptions' => $inscriptions,
         ]);
     }
 }
---
+++ src/Form/UtilisateurType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('nom')
+            ->add('prenom')
+            ->add('naissance')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
